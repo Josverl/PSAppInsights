34676effbf2cf97ad9bb54ad92a1c4966989cbd5
Jos Verlinde <jos_verlinde@hotmail.com>
2016-11-12 01:00:46 +0100

    Add Documented samples for performance counters and Live Metrics

---
+++ Sample/Live Metrics.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+    Starts logging a live metrics stream to App Insights
+.DESCRIPTION
+    The logging stream sends a standard set of perfcounters every second, which allows you to monitor essential counters directly in AppInsights.
+    
+    Please note that this will incur a continous stream of performance and statictics to the azure AppInsights portal,
+    this is in the format of a HTTPS traffic stream which will send about 5 to 6 MBytes per hour to https://rt.services.visualstudio.com
+
+    This information is sent asynchronously from the powershell script or module and requires no interaction other that starting or stopping.
+    Please note that there is only a single LiveMetrics view for each Instrumentation key.
+    > If multiple instances of the same script log to the same Key the counters will be combined
+    > if you need to view seperate LiveMetrics, you need to instrument your scripts with different keys.
+#>
+
+#Instrumentation key 
+$key = "c90dd0dd-3bee-4525-a172-ddb55873d30a"
+
+#start logging metrics for this powershell process 
+Start-AILiveMetrics -Key $key 
+
+1..10 | % {
+    $mem_stress = @()
+    1..6 | %{
+        Write-Host "Eat..." -NoNewline
+        #Eat some memory
+        for ($i = 0; $i -lt 5000; $i++) { $mem_stress += ("a" * 200MB) }
+        Write-Host "Sleep..." -NoNewline
+        Sleep -Seconds 2
+    }
+    Remove-Variable mem_stress
+    Write-Host "." 
+}        
+
+Stop-AILiveMetrics
---
+++ Sample/PerformanceCounters.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+    Start collection perfromance counters and send them to App Insights
+
+    The collection includes perf counters of the powershell process tha runs the script.
+   
+    By default the following perf counters are collected : 
+    - Handle Count 
+    - Working Set 
+
+    If other process counters are to be collected these can be specified using -ProcessCounters
+    (currently only process counters are supported)
+
+    The perfmon collector is mantained per (powershell) process, and a reference to the collectore is stored in a global variable.
+    $Global:AISingleton.PerformanceCollector
+
+    By default the counters are collected and sent every 30 seconds
+#> 
+
+#Instrumentation key 
+$key = "c90dd0dd-3bee-4525-a172-ddb55873d30a"
+
+#Start logging the default set of perf counters 
+Start-AIPerformanceCollector -Key $key
+
+#get list of all process counters  
+$ProcessCounters = (Get-Counter -ListSet "Process").paths | %{ $_.Split('\')[2]}
+
+#Start logging
+Start-AIPerformanceCollector -Key $key -ProcessCounters $ProcessCounters
+
+#let the counters run for 10 minutes 
+Start-Sleep -Seconds 10*60
+
+#Stop sending the counters
+Stop-AIPerformanceCollector
