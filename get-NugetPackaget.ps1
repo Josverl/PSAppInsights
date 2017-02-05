1fb23fc8acdc3fee7bfcab413410ce7329967429
Jos Verlinde[MSFT] <jos_verlinde@hotmail.com>
2017-02-05 11:43:21 -0800

    Add Package configuration and script

---
+++ get-NugetPackaget.ps1
@@ -0,0 +1,7 @@
+﻿#Assumes current folder to be the location where the packages are to be installed
+
+#Make sure to update Nuget 
+.\nuget.exe update -self
+#Now install the packages as specified 
+.\nuget.exe install .\packages.config -o . 
+
