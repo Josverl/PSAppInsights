c375a3169260fc6c7eb2b8985b18024eee02650a
Jos Verlinde[MSFT] <jos_verlinde@hotmail.com>
2017-04-20 08:17:52 +0200

    move build script simplyfiers

--- Build.ps1
+++
@@ -1,5 +0,0 @@
-Write-host -ForegroundColor Yellow  'Run the normal build and test suite using the DEV repro'
-invoke-psake .\default.ps1
-
-Get-Module psappinsights
-Get-InstalledModule psappinsights -AllVersions | FT
--- Publish.ps1
+++
@@ -1,6 +0,0 @@
-Write-host -ForegroundColor Yellow  'Run the normal build and test, in finish by publishing to PSGallery'
-invoke-psake .\default.ps1 -taskList Publish, Install
-
-Get-Module psappinsights
-Get-InstalledModule psappinsights -AllVersions
-
